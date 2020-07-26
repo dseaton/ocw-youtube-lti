9b343d9039a93e40bfed7fe28f6d4fc549fdf728
Jean-Michel Claus <jmc@math.mit.edu>
2020-07-26 17:59:55 +0200

    Add YouTube Terms of Service and Google Privacy Policy in splash info

--- client/src/components/Context.js
+++ client/src/components/Context.js
@@ -5,7 +5,7 @@ export default function Context() {
   return (
     <div className="context">
       <p>
-        <span className="footer__text-2">Welcome to the OpenCourseWare YouTube LTI Sandbox.</span> 
+        <strong>OCW YouTube LTI Sandbox</strong> 
         <br/>
         We are gathering input from users on how best to integrate OCW into on-campus LMSs. 
         This application is under active development and may be down from time to time. 
@@ -15,7 +15,11 @@ export default function Context() {
           <li>One click embed of an OCW video directly in a Canvas page.</li>
         </ul>
       </p>
-      <p>Type a term into the search bar to get started.</p>
+      <p>Contact us if you have more feedback, ideas, or other needs.</p>
+      <p>
+        <a className="context__link" href="https://www.youtube.com/t/terms">YouTube Terms of Service</a>
+        <a className="context__link" href="https://policies.google.com/privacy">Google Privacy Policy</a>
+      </p>
     </div>
   );
 }
