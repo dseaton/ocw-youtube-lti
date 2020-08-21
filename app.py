be7aae6731e0a32572bdae466d6b876e03ee8f89
Daniel Seaton <dseaton@Daniels-MBP-66.hsd1.ma.comcast.net>
2020-08-21 12:19:42 -0400

    forgot to turn LTI back on, but, samesite None worked. Trying in canvas now

--- app.py
+++ app.py
@@ -30,15 +30,15 @@ def hello_world(lti=lti):
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET'])
-# @lti(request='initial', error=error, app=app)
-# def index(lti=lti):
-def index():
+@lti(request='initial', error=error, app=app)
+def index(lti=lti):
+# def index():
     return app.send_static_file('index.html')
 
 @app.route('/search', methods=['GET', 'POST'])
-# @lti(request='session', error=error, app=app)
-# def search(lti=lti):
-def search():
+@lti(request='session', error=error, app=app)
+def search(lti=lti):
+# def search():
     search_url = 'https://www.googleapis.com/youtube/v3/search'
     video_url = 'https://www.googleapis.com/youtube/v3/videos'
     caption_url = 'https://www.googleapis.com/youtube/v3/captions/'
